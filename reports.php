83159cb1460da2ccfe4dfbf455fa70e79f06f5f7
Amner Saucedo Sosa <waptoing7@gmail.com>
2017-07-15 16:52:40 -0600

    V1.0

--- reports.php
+++ reports.php
@@ -17,7 +17,7 @@
                 <div class="col-md-12 col-sm-12 col-xs-12">
                     <div class="x_panel">
                         <div class="x_title">
-                            <h2>Usuarios</h2>
+                            <h2>Reportes</h2>
                             <ul class="nav navbar-right panel_toolbox">
                                 <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                                 </li>
